2ade5351ae6893519ca29751579719457c3b8209
saindou2001 <soilhatisaindou@gmail.com>
2023-02-02 09:08:44 +0100

    enregistrement des valeurs saisies

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let tabResult = new Array();
 
+let tabSaisi = new Array();
 
+function enregistrementValeur() {
+    for (let index = 0; index < 7; index++)
+    {
+        tabSaisi[index] = document.getElementById(String(index)).value;        
+    }
+    console.log(tabSaisi);
+}
 function resultAleatoire() {
     
     for(let i =0;i<7;i++){
